2787d9999b89dd79eb7efc923af115d273ed1f38
JalilAhmad456 <JalilAhmad223355@gmail.com>
2024-11-08 12:10:18 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,1 +1,45 @@
+import React from "react";
 
+function About() {
+  return (
+    <div className="max-w-2xl mx-auto text-left p-4">
+      <h2 className="text-xl font-medium mb-2">About this app</h2>
+      <p className="text-gray-600 ml-2">
+        About this app Online video and voice chat application. New Girl is an
+        online video and voice chat application that allows you to make friends
+        from all over the world. Through video calls, you can communicate
+        face-to-face with strangers on video anytime, anywhere. Come to New Girl
+        now to make new friends! Users in more than 100 countries are waiting
+        for you!
+        <br />
+        <br />
+        New Girl has the following functions:
+        <br />
+        Real-time video call chat:
+        <br />
+        •⁠ ⁠﻿You can video chat with people from all over the world by random
+        matching or actively dialing videos, and meet new friends through smooth
+        and clear video calls.
+        <br />
+        •⁠ ⁠﻿Discover new friends you are interested in and enjoy an interesting
+        video chat experience!
+        <br />
+        <br />
+        Chat real-time translation:
+        <br />
+        -You can chat with people abroad without mastering a foreign language.
+        The real-time translation function can solve the problem of language
+        barriers in the chat.
+        <br />
+        To ensure the authenticity of the anchor:
+        <br />
+        •⁠ ⁠﻿We strictly combat fake photos, and every photo will be reviewed
+        <br />
+        •⁠ ⁠﻿We guarantee that all online users are online in real time, and
+        most people can answer video calls in just a few seconds.
+      </p>
+    </div>
+  );
+}
+
+export default About;
