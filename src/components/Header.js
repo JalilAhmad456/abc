f9c6e4a2ef90960f87d5c0f00648b3fea97cbd51
JalilAhmad456 <JalilAhmad223355@gmail.com>
2024-11-08 12:45:36 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import logo from "../logo.jpeg";
 function Header() {
   return (
     <div className="p-4 max-w-2xl mx-auto p-4">
-      <div className="flex justify-between items-center p-2">
+      <div className="flex justify-around items-center p-2">
         <img
           src={logo}
           alt="Rummy Cafe Blast Logo"
